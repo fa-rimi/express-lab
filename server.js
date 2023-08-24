325c73071471bfa560e0d1c1099c5537c957631d
rimi <a.fatimaarimi@gmail.com>
2023-08-24 14:45:33 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -29,7 +29,37 @@ app.get("/greeting/:name", (req, res) => {
 // Extract the 'total' and 'tipPercentage' parameters from the URL.
 app.get("/tip/:total/:tipPercentage", (req, res) => {
   let tip = req.params.total * (req.params.tipPercentage / 100);
-  res.send(`Tip Amount: $${tip}`);
+  res.send(`Tip Amount: $${tip} for ${req.params.total}`);
+});
+
+// todo: Magic 8 Ball
+app.get("/magic/:question", (req, res) => {
+  const magicRes = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  const magicRandRes = magicRes[Math.floor(Math.random() * magicRes.length)];
+  res.send(`
+    <h1>${req.params.question}</h1> 
+    ${magicRandRes}`);
 });
 
 // Start the server
